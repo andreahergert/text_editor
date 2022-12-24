af78fd852153c1e16029bf9e1e9e00219af40a92
Andrea Hergert <ahergert24@hotmail.com>
2022-12-23 20:20:46 -0800

    added comments

--- client/src-sw.js
+++ client/src-sw.js
@@ -26,7 +26,7 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
+// Implements asset caching
 registerRoute(({ request }) => ['style', 'script', 'worker'].includes(request.destination),
 new CacheFirst({
   cacheName: 'asset-cache',
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,8 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
@@ -42,6 +40,8 @@ module.exports = () => {
       }),
     ],
 
+// configured workbox plugins for a service worker and manifest file.
+// CSS loaders and babel to webpack.
     module: {
       rules: [
         {
